e8f16d8479f7f2bd034d627c85043adee5a518e0
Arvid Mustonen <juhoarvid+github at gmail dot com>
2019-07-25 06:16:49 +0300

    WIP:Check category of certain transaction source

--- ParseBankStatement.py
+++ ParseBankStatement.py
@@ -1,6 +1,10 @@
 import csv
 import collections
 import argparse
+import json
+
+with open('nordea_categories.json', 'r') as handle:
+    CATEGORIES = json.load(handle)
 
 class NordeaTransactionLine(object):
     def __init__(self, Kirjauspaiva, Arvopaiva, Maksupaiva, Maara,
@@ -34,14 +38,33 @@ def get_csv_as_dict(path_to_csv):
                 pass
     return csv_list
 
-def categorize_transaction(self, date, amount, source):
-    pass
+def check_category(source, categorize=True):
+    """
+    Check if source has an defined category.
+
+    Params
+    source  int     transaction source
+    categorize  bool    If True, request category for not defined transaction
+                        otherwise return category "Other" for those
+    """
+    other="Other"
+
+    category = [key for key, value in CATEGORIES.items() if source in value]
+    if category == []:
+        return other
+    else:
+        return category[0]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse bank transactions')
     parser.add_argument('csvfile', type=str,
                         help='path to transaction csv file')
     args = parser.parse_args()
-    print(get_csv_as_dict(args.csvfile))
+    csv = get_csv_as_dict(args.csvfile)
+    for ta in csv:
+        print(ta)
+        print(type(ta))
+        print(ta['source'])
+        print(check_category(source=ta['source']))
 
 
