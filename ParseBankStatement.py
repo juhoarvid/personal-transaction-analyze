b3c84f1f030f148d8505e624e22318364616f32d
Arvid Mustonen <juhoarvid+github at gmail dot com>
2019-07-26 09:45:32 +0300

    WIP: Ability to read and write to given configuration file

--- ParseBankStatement.py
+++ ParseBankStatement.py
@@ -1,10 +1,9 @@
 import csv
 import collections
-import argparse
+import optparse
 import json
 
-with open('nordea_categories.json', 'r') as handle:
-    CATEGORIES = json.load(handle)
+
 
 class NordeaTransactionLine(object):
     def __init__(self, Kirjauspaiva, Arvopaiva, Maksupaiva, Maara,
@@ -20,75 +19,95 @@ conf = {
         'transcation_field_parser' : NordeaTransactionLine
     }
 }
+class categorize(object):
+    conf = {
+        'nordea' : {
+            'first_transaction_line' : 5,
+            'transcation_field_parser' : NordeaTransactionLine
+        }
+    }
+    def __init__(self, categoryinputfile):
+        with open(categoryinputfile, 'r') as handle:
+            self.CATEGORIES = json.load(handle)
 
-def get_csv_as_dict(path_to_csv):
-    csv_list = []
-    with open(path_to_csv, mode='r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter='\t')
-        csvline=1
-        for row in csv_reader:
-            if csvline < conf['nordea']['first_transaction_line']:
-                csvline+=1
-                continue
-            try:
-                csv_list.append(((conf['nordea']['transcation_field_parser'](*row).__dict__)))
-            except TypeError:
-                if row != []:
-                    print("PASS:{}".format(row))
-                pass
-    return csv_list
+    def get_csv_as_dict(self, path_to_csv):
+        csv_list = []
+        with open(path_to_csv, mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter='\t')
+            csvline=1
+            for row in csv_reader:
+                if csvline < conf['nordea']['first_transaction_line']:
+                    csvline+=1
+                    continue
+                try:
+                    csv_list.append(((conf['nordea']['transcation_field_parser'](*row).__dict__)))
+                except TypeError:
+                    if row != []:
+                        print("PASS:{}".format(row))
+                    pass
+        return csv_list
 
-def check_category(source, categorize=True):
-    """
-    Check if source has an defined category.
+    def check_category(self, source, categorize=True):
+        """
+        Check if source has an defined category.
 
-    Params
-    source  int     transaction source
-    categorize  bool    If True, request category for not defined transaction
-                        otherwise return category "Other" for those
-    """
-    other="Other"
+        Params
+        source  int     transaction source
+        categorize  bool    If True, request category for not defined transaction
+                            otherwise return category "Other" for those
+        """
+        other="Other"
 
-    category = [key for key, value in CATEGORIES.items() if source in value]
-    if category == []:
-        if categorize:
-            request = {str(i):key for i,key in enumerate(CATEGORIES.keys())}
-            request["A"] = "ADD NEW CATEGORY"
-            selection = input("For which category '{}' should be:{}".format(
-                source, request
-            ))
-            if selection not in request.keys():
-                print("Wrong selection! '{}' not in '{}'".format(
-                    selection, request.keys()
+        category = [key for key, value in self.CATEGORIES.items() if source in value]
+        if category == []:
+            if categorize:
+                request = {str(i):key for i,key in enumerate(self.CATEGORIES.keys())}
+                request["A"] = "ADD NEW CATEGORY"
+                selection = input("For which category '{}' should be:{}".format(
+                    source, request
                 ))
-                return check_category(source, categorize)
-            elif selection == "A":
-                new_category=input("Give new gategory name:")
-                print("New category '{}' created".format(new_category))
-                CATEGORIES[new_category] = []
-                return new_category
-            else:
-                return request[selection]
-    else:
-        return category[0]
+                if selection not in request.keys():
+                    print("Wrong selection! '{}' not in '{}'".format(
+                        selection, request.keys()
+                    ))
+                    return self.check_category(source, categorize)
+                elif selection == "A":
+                    new_category=input("Give new gategory name:")
+                    print("New category '{}' created".format(new_category))
+                    self.CATEGORIES[new_category] = []
+                    return new_category
+                else:
+                    return request[selection]
+        else:
+            return category[0]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Parse bank transactions')
-    parser.add_argument('csvfile', type=str,
-                        help='path to transaction csv file')
-    args = parser.parse_args()
-    csv = get_csv_as_dict(args.csvfile)
+    parser = optparse.OptionParser(description='Parse bank transactions')
+    parser.add_option('-c', '--configuration', action='store',
+                        dest='configurationfilepath',
+                        default="nordea_categories.json",
+                        help='configuration file')
+    parser.add_option('-i', '--input', action='store',
+                        dest='inputfile',
+                        help='Transaction csv file path')
+    parser.add_option('-o', '--output', action='store',
+                        dest='outputcatfile',
+                        default="nordea_categories.json",
+                        help='New categories will be added to this file')
+    (options, args) = parser.parse_args()
+    cat = categorize(options.configurationfilepath)
+    csv = cat.get_csv_as_dict(options.inputfile)
     for i, ta in enumerate(csv):
-        category_known = [i for key, value in CATEGORIES.items() if ta in value]
+        category_known = [i for key, value in cat.CATEGORIES.items() if ta in value]
         if category_known != []:
             continue
-        category = check_category(source=ta['source'])
+        category = cat.check_category(source=ta['source'])
         print(category)
-        CATEGORIES[category].append(ta['source'])
+        cat.CATEGORIES[category].append(ta['source'])
         if i == 10:
             break
-    print(CATEGORIES)
-    with open('cat_output.json', 'w') as handle:
-        handle.write(json.dumps(CATEGORIES, indent=4, sort_keys=True))
+    print(cat.CATEGORIES)
+    with open(options.outputcatfile, 'w') as handle:
+        handle.write(json.dumps(cat.CATEGORIES, indent=4, sort_keys=True))
 
 
